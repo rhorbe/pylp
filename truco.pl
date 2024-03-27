75fe81b699fc06477afd5461034e2c29cb394be8
Rafael Orbe <rafaelorbe@outlook.com>
2024-03-27 00:15:38 -0300

    Trabajado en envido

--- truco.pl
+++ truco.pl
@@ -19,19 +19,30 @@ resultado(Carta1, Carta2, Resultado) :-
 % Dada una mano indique su valor de envido
 
 envido(Mano, Valor) :-
+ %validar cartas ingresadas
  hayEnvido(Mano),
- mejoresCartasEnvido(Mano, MejorCarta1, MejorCarta2),
- valorEnvido(MejorCarta1, MejorCarta2, Valor).
+ cartasEnvido(Mano, CartaEnvido1, CartaEnvido2),
+ valorEnvido(CartaEnvido1, CartaEnvido2, Valor).
 
 hayEnvido([carta(_, Palo1), carta(_, Palo2), carta(_, Palo3)]) :- 
   Palo1 == Palo2;
   Palo2 == Palo3;
   Palo3 == Palo1.
 
-mejoresCartasEnvido([carta(Numero1, Palo1), carta(Numero2, Palo2), carta(Numero3, Palo3)], MejorCarta1, MejorCarta2) :-
-    MejorCarta1 = carta(Numero1, Palo1), 
-    MejorCarta2 = carta(Numero2, Palo2).
+hayFlor([carta(_, Palo1), carta(_, Palo2), carta(_, Palo3)]) :- 
+  Palo1 == Palo2,
+  Palo1 == Palo3.
 
-% valorEnvido(Carta1, Carta3, Valor) :- Valor = 20.
-valorEnvido(_, _, Valor) :-
-    Valor = 20.
+mismoPalo(carta(_, Palo1), carta(_, Palo2)) :- Palo1 == Palo2.
+
+cartasEnvido([Carta1, Carta2, Carta3], CartaEnvido1, CartaEnvido2) :-
+    hayFlor([Carta1, Carta2, Carta3]), 
+    mismoPalo(Carta1, Carta2),  CartaEnvido1 = Carta1, CartaEnvido2 = Carta2, 
+    mismoPalo(Carta2, Carta3),  CartaEnvido1 = Carta2, CartaEnvido2 = Carta3, 
+    mismoPalo(Carta3, Carta1),  CartaEnvido1 = Carta3, CartaEnvido2 = Carta1, 
+
+valorEnvido(carta(Numero1, _), carta(Numero2, _), Valor) :-
+    (7 >= Numero1, 7 >= Numero2, Valor is Numero1 + Numero2 + 20); 
+    (7 <  Numero1, 7 >= Numero2, Valor is Numero2 + 20);
+    (7 >= Numero1, 7 <  Numero2, Valor is Numero1 + 20);
+    Valor is 20.
