f1f91cd0f5faa85d0b825a76f5be9718348a8985
Rafael Orbe <rafaelorbe@outlook.com>
2024-04-05 18:46:18 -0300

    Entrega Final

--- truco/cartas.pl
+++ truco/cartas.pl
@@ -1,5 +1,5 @@
 % Cartas
-/* carta(1, espada).
+carta(1, espada).
 carta(2, espada).
 carta(3, espada).
 carta(4, espada).
@@ -19,7 +19,7 @@ carta(6, basto).
 carta(7, basto).
 carta(10,basto).
 carta(11,basto).
-carta(12,basto).*/ 
+carta(12,basto).
 
 carta(1, oro).
 carta(2, oro).
@@ -28,7 +28,7 @@ carta(4, oro).
 carta(5, oro).
 carta(6, oro).
 carta(7, oro).
-carta(10,oro). /* 
+carta(10,oro).
 carta(11,oro).
 carta(12,oro).
 
@@ -42,4 +42,3 @@ carta(7, copa).
 carta(10,copa).
 carta(11,copa).
 carta(12,copa).
- */
